eec86908c7405cb730e426b3ce61c3d50cd8d837
Your Name <zohaibabbas8557@gmail.com>
2024-08-29 12:49:55 +0500

    targetign the element by some tagsnames

--- Target-By-TagName/script.js
+++ Target-By-TagName/script.js
@@ -5,3 +5,13 @@ function styledPara(){
         para[i].classList.add('styled');
     }
 }
+
+function someStyledTag() {
+    let e = document.getElementById('container-1');
+    //  here we write contianer-2 then it will target the p tags of contianer-2
+    let para = e.getElementsByTagName('p');
+    for (let i=0; i < para.length; i++){
+        para[i].style.color = 'purple';
+        para[i].style.backgroundColor ='gray'
+    }
+}
