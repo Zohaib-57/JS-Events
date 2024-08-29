249c96fbf83802a0b7bf3e5284a96d640115c6ac
Your Name <zohaibabbas8557@gmail.com>
2024-08-29 10:37:06 +0500

    swapping images and selecting the classes

--- Swapping-images/script.js
+++ Swapping-images/script.js
@@ -1,4 +1,12 @@
-function swapImage(newImage){
+function swapImage(newImage,clickedImage){
     let selectedImage = document.getElementById("selected-img");
   selectedImage.src = newImage;
+
+
+// applying the classes
+ let newFeature = document.querySelectorAll(".gallery img");
+ newFeature.forEach(img => img.classList.remove("active"));
+
+
+ clickedImage.classList.add("active");
 }
